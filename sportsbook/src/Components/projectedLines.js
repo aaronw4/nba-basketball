0d4dec2d7322183869b38204a1df338f4739ab5e
Aaron Williams <aaronrw4@yahoo.com>
2021-01-13 22:09:24 -0800

    Added projectedLines

---
+++ sportsbook/src/Components/projectedLines.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {findTeam} from './findTeam';
+import {projected} from './projected';
+
+const ProjectedLines = (props) => {
+    let awayStats = findTeam(props.away);
+    let homeStats = findTeam(props.home);
+    let projectedStats = projected(awayStats[0], homeStats[0]);
+    
+    return (
+        <div className='fgOpening'>
+            <h5>Full Game Projected</h5>
+            <p>Away: {projectedStats[0].spreadAway}</p>
+            <p>Home: {projectedStats[0].spreadHome}</p>
+            <br/>
+            <p>Away: {projectedStats[0].moneylineAway}</p>
+            <p>Home: {projectedStats[0].moneylineHome}</p>
+            <br/>
+            <p>Over {projectedStats[0].total}</p>
+            <p>Under {projectedStats[0].total}</p>
+        </div>
+    )
+}
+
+export default ProjectedLines
