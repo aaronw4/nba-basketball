2879bc2891684afcd1204a44cf10fed28f0cae65
Aaron Williams <aaronrw4@yahoo.com>
2021-04-01 10:45:38 -0700

    Added Routes and Links to App

--- sportsbook/src/App.js
+++ sportsbook/src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-import GameLines from './Components/gameLines'
+import {Route, Link} from 'react-router-dom';
+import GameLines from './Components/gameLines';
+import Results from './Components/results';
 
 function App() {
   return (
     <div className="App">
-      <GameLines/>
+      <Route exact path='/'>
+        <Link to='/results'>
+          <h3>Results</h3>
+        </Link>
+        <GameLines/>
+      </Route>
+      <Route path='/results'>
+        <Results/>
+      </Route>
     </div>
   );
 }
