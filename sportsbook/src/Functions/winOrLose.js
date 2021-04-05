4d97dfd12be73fb24ad4ffcbf5016130e323472b
Aaron Williams <aaronrw4@yahoo.com>
2021-04-05 12:40:36 -0700

    Created win or lose fxns

---
+++ sportsbook/src/Functions/winOrLose.js
@@ -0,0 +1,49 @@
+export function WinOrLoseSpread(awayScore, homeScore, pick, pickSpread) {
+    let gameResult
+    let result
+
+    if (pick === 'Away') {
+        gameResult = Number(homeScore) - Number(awayScore)
+    } else if (pick === 'Home') {
+        gameResult = Number(awayScore) - Number(homeScore)
+    }
+    
+    if (pick === 'No bet') {
+        result = ''
+    } else if (Number(pickSpread) - Number(gameResult) > 0) {
+        result = 'Win'
+    } else if (Number(pickSpread) - Number(gameResult) < 0) {
+        result = 'Lose'
+    } else {
+        result = 'Push'
+    }
+
+    return result
+}
+
+export function WinOrLoseTotal(awayScore, homeScore, pick, total) {
+    let gameResult = Number(awayScore) + Number(homeScore)
+    let result
+    
+    if (pick === 'No bet') {
+        result = ''
+    } else if (pick === 'Over') {
+        if (Number(gameResult) - Number(total) > 0) {
+            result = 'Win'
+        } else if (Number(gameResult) - Number(total) < 0) {
+            result = 'Lose'
+        } else {
+            result = 'Push'
+        }
+    } else if (pick === 'Under') {
+        if (Number(total) - Number(gameResult) > 0) {
+            result = 'Win'
+        } else if (Number(total) - Number(gameResult) < 0) {
+            result = 'Lose'
+        } else {
+            result = 'Push'
+        }
+    }
+
+    return result
+}
